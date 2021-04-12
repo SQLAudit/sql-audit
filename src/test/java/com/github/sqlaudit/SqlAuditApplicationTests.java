91fa90482eaf3ba775a85ea36b854841afc53d01
Jin Wang <15305187826@189.cn>
2021-04-12 16:42:39 +0800

    inti commit

---
+++ src/test/java/com/github/sqlaudit/SqlAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sqlaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
